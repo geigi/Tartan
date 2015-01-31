75d19c19ed4e64bbfcbb93fad4d2b80f8d9072d0
SMiTTY <mi@zaturox.de>
2015-01-31 14:48:26 +0100

    explude python cache files from version control

--- gallery/urls.py
+++ gallery/urls.py
@@ -10,4 +10,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include ('PhotoGallery.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root='D:\\Users\\SMiTTY\\Desktop\\gallery\\PhotoGallery\\static\\')
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
