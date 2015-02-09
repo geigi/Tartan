358130599705adeaa7e774a2aa71dbe84c15e152
SMiTTY <mi@zaturox.de>
2015-02-09 17:50:04 +0100

    changes image in Carousel JavaScript-based. Therefore new Django view to return neccessary info in JSON

--- PhotoGallery/urls.py
+++ PhotoGallery/urls.py
@@ -5,5 +5,6 @@ from PhotoGallery import views
 urlpatterns = [
     url(r'^detailed/(?P<imgid>[0-9]+)/$', views.imageDetail, name='imageDetail'),
     url(r'^album/(?P<albumid>[0-9]+)/$', views.albumDetail, name='albumDetail'),
+    url(r'^detailed/(?P<imgid>[0-9]+)/info.json$', views.imageJsonInfo, name='imageJsonInfo'),
     url(r'^$', views.overview, name='albumIndex'),
 ]
