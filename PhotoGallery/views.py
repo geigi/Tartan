f8621c5084b9a6641ae7eb3b9159acc9ad623201
zaturox <github@zaturox.de>
2015-02-25 10:59:33 +0100

    code cleanups

--- PhotoGallery/views.py
+++ PhotoGallery/views.py
@@ -5,53 +5,48 @@ from PhotoGallery.models import Album, Photo
 
 import json
 
+ASCENDING = False;
+DESCENDING = True;
+
+####### USER VIEWS #######
+
+# Home view (start page) -> list all albums
 def overview(request):
     albumList = Album.objects.order_by('name')
     context = {'albumList': albumList}
     return render(request, 'PhotoGallery/gallery.thtm', context)
 
-    
+# Album view -> list all images inside an album
 def albumDetail(request, albumid):
     try:
         album = Album.objects.get(id=albumid)
     except Album.DoesNotExist:
         raise Http404
     
-    def sortparse(rawval):
-        return {
-            'oldest':'-added',
-            'newest':'added',
-            'name':'name',
-            'namereverse':'-name',
-            }.get(rawval)
-    
-    orderParam = request.GET.get('ordering')
-    ordering = sortparse(orderParam)
+    ordering = sortparse(request)
      
-    imgSet = album.photo_set #do not pass this to template, its not iterable!
-    
-    if (ordering):
-        imgList = imgSet.order_by(ordering, 'pk')
-    else:
-        imgList = imgSet.all
-    
+    imgList = album.photo_set.order_by(ordering[2], 'pk') #do not pass this to template, its not iterable!
+        
     context = {'album': album, 'imgList': imgList}
 
     return render(request, 'PhotoGallery/album.thtm', context)
 
+# Detail view -> show a single image in large size
 def imageDetail(request, imgid):
     try:
         img = Photo.objects.get(id=imgid)
     except Photo.DoesNotExist:
         raise Http404
-        
+    
+    # get next element
     q = img.album.photo_set.filter(id__gt=imgid).order_by('id')[:1]
     if (q):
         next = q[0]
     else:
         next = False
     
-    q = img.album.photo_set.filter(id__lt=imgid).order_by('-id')[:1]
+    # get previous element
+    q = img.album.photo_set.filter(id__lt=imgid).order_by('id').reverse()[:1]
     if (q):
         prev = q[0]
     else:
@@ -67,7 +62,11 @@ def imageDetail(request, imgid):
     
     context = {'img':img, 'next':next, 'prev':prev, 'diaDuration': dur}
     return render(request, 'PhotoGallery/carousel.thtm', context)
-    
+
+
+####### API VIEWS #######
+
+# return image specific information
 def imageJsonInfo (request, imgid):
     try:
         img = Photo.objects.get(id=imgid)
@@ -111,3 +110,16 @@ def imageJsonInfo (request, imgid):
     
     
     return HttpResponse(json.dumps(jsonInfo), content_type="application/json")
+
+####### HELPER FUNCTONS #######
+
+# return a tupel defining the ordering.
+# always returns a tupel, never false -> no eror checking needed
+def sortparse(request):
+    rawval = request.GET.get('ordering')
+    return {
+        'oldest':('added', DESCENDING, '-added'),
+        'newest':('added', ASCENDING, 'added'),
+        'name':('name', DESCENDING, 'name'),
+        'namereverse':('name',ASCENDING, '-name'),
+        }.get(rawval, ('pk', ASCENDING, 'pk'))      # default: order by primary key (pk)
