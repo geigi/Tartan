59fe2166f697560293346b48a3b9781aef7b10d3
Geigi <git@geigi.de>
2015-03-02 15:18:22 +0100

    Order by in carousel

--- PhotoGallery/static/js/gallery.js
+++ PhotoGallery/static/js/gallery.js
@@ -1,9 +1,29 @@
 function init() {
-	in
+	initDropdown();
 }
 
 function displayAlbums() {
 	$(function() {
 		$(".panel").fadeToggle(0).fadeToggle(300);
 	}); 
+}
+
+function initDropdown() {
+	ordering = getGet('ordering');
+	dropdown = document.getElementById("ordering");
+
+	switch(ordering) {
+		case 'name':
+			dropdown.options[0].selected = true;
+			break;
+		case 'namereverse':
+			dropdown.options[1].selected = true;
+			break;
+		case 'newest':
+			dropdown.options[2].selected = true;
+			break;
+		case 'oldest':
+			dropdown.options[3].selected = true;
+			break;
+	}
 }
